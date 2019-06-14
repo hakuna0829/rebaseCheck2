90e222aa04bc79c5f41e4449953dfdf654260772
David <david@outlier.org>
2019-06-14 13:26:16 -0700

    feat: add user-agent endpoint

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ function respondBase64 (req, res) {
   const phrase = req.url.replace(/^\/b64\//, '')
   res.end(JSON.stringify({ b64: Buffer.from(phrase).toString('base64') }))
 }
+function respondUserAgent (req, res) {
+  const ua = req.headers['user-agent']
+  res.end(JSON.stringify({ ua }))
+}
+
 
 server.listen(PORT)
 console.log(`Server listening on port ${PORT}`)
